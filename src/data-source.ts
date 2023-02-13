7b8fc6b335ced8aecce1772a649a7fad25660b73
Assel Zholdasbekova <asselzholdasbekova@Assels-MacBook-Pro.local>
2023-02-13 15:28:41 +0600

    Connect to deployed database

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,12 @@ import { Genre } from "./entity/Genre"
 import { Song } from "./entity/Song"
 
 export const AppDataSource = new DataSource({
+    name: "default",
     type: "postgres",
-    host: "localhost",
+    url: "postgres://cdxmntqf:y6xVhDs2uCIpZOu1YY7cYB_-p3GR8oic@trumpet.db.elephantsql.com/cdxmntqf",
     port: 5432,
-    username: "postgres",
-    password: "admin",
+    username: "cdxmntqf",
+    password: "y6xVhDs2uCIpZOu1YY7cYB_-p3GR8oic",
     database: "express_intro",
     synchronize: true,
     logging: false,
